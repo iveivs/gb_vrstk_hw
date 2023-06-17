d6c869726e96e888d986fcceced79991ea3c2a66
Svetoslav <veiv@mail.ru>
2023-06-17 10:27:57 +0300

    17 june

---
+++ HW_3/js/script.js
@@ -0,0 +1,27 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//     const menu = document.querySelector('.nav'),
+//     menuItem = document.querySelectorAll('.menu_item'),
+//     hamburger = document.querySelector('.hamburger');
+
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('hamburger_active');
+//         menu.classList.toggle('menu_active');
+//     });
+
+//     menuItem.forEach(item => {
+//         item.addEventListener('click', () => {
+//             hamburger.classList.toggle('hamburger_active');
+//             menu.classList.toggle('menu_active');
+//         })
+//     })
+// })
+
+// - - - - - - - 
+// вариант 2
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
